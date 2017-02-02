69851366db1a78670a41f5419ea8b7ef96703d57
Hilary Lewis <hilarylewis92@gmail.com>
2017-02-01 20:28:47 -0700

    On click of option button, log inner text in console

--- public/poll/poll.js
+++ public/poll/poll.js
@@ -1,11 +1,5 @@
 const socket = io();
 
-const connectionCount = document.getElementById('connection-count');
-
-socket.on('usersConnected', (count) => {
-  connectionCount.innerText = 'Connected Users: ' + count;
-});
-
 $(document).ready(function() {
   const pollId = getParameterByName('pollId')
 
@@ -27,9 +21,30 @@ function getParameterByName(name, url) {
 function getPollData(pollId) {
   $.get(`/api/poll/${pollId}`, function(data) {
     $('.poll-question').append(`<h3>${data.info.poll.question}</h3>`)
-    $('.poll-option-1').append(`<input type='submit' value='${data.info.poll.options[0].option}'/>`)
-    $('.poll-option-2').append(`<input type='submit' value='${data.info.poll.options[1].option}'/>`)
-    $('.poll-option-3').append(`<input type='submit' value='${data.info.poll.options[2].option}'/>`)
-    $('.poll-option-4').append(`<input type='submit' value='${data.info.poll.options[3].option}'/>`)
+    $('.option-1').append(data.info.poll.options[0].option)
+    $('.option-2').append(data.info.poll.options[1].option)
+    $('.option-3').append(data.info.poll.options[2].option)
+    $('.option-4').append(data.info.poll.options[3].option)
+  });
+}
+
+const connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', (count) => {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+const statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', (message) => {
+  statusMessage.innerText = message;
+});
+
+const buttons = document.querySelectorAll('#choices button');
+console.log(buttons);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    console.log(this.innerText);
   });
 }
